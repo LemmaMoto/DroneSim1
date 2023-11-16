2173d6e2d06a90df9aea9be3b12320d6dc52cb1b
LemmaMoto <emanuelebuaodetti@gmail.com>
2023-11-16 12:23:42 +0100

    cose

---
+++ WatchdogWithoutPipes17_10/build/CMakeFiles/child.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ema/Documents/Unige/ARP/DroneSim1/WatchdogWithoutPipes17_10/child.c" "CMakeFiles/child.dir/child.c.o" "gcc" "CMakeFiles/child.dir/child.c.o.d"
+  "/home/ema/Documents/Unige/ARP/DroneSim1/WatchdogWithoutPipes17_10/utility/wrapfunc.c" "CMakeFiles/child.dir/utility/wrapfunc.c.o" "gcc" "CMakeFiles/child.dir/utility/wrapfunc.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ WatchdogWithoutPipes17_10/build/CMakeFiles/watchdog.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../watchdog"
+  "../watchdog.pdb"
+  "CMakeFiles/watchdog.dir/utility/wrapfunc.c.o"
+  "CMakeFiles/watchdog.dir/utility/wrapfunc.c.o.d"
+  "CMakeFiles/watchdog.dir/watchdog.c.o"
+  "CMakeFiles/watchdog.dir/watchdog.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/watchdog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
