a36f394d6ba8e9c5f7d3396447313d70055cfcd0
FedeMala00 <feffemala@gmail.com>
2023-11-27 17:57:08 +0100

    added cosde

--- Imple/drone.c
+++ Imple/drone.c
@@ -14,12 +14,12 @@
 #include <sys/shm.h>
 #include <math.h>
 
-#define SHM_DRN 12
+#define SHM_DRN 12 // Define a shared memory key
 
-#define PIPE_READ 0
-#define PIPE_WRITE 1
+#define PIPE_READ 0  // Define the read end of the pipe
+#define PIPE_WRITE 1 // Define the write end of the pipe
 
-#define T 0.01
+#define T 0.01 // Define a time constant
 
 // Initialize position and velocity
 double x = 0, y = 0;
@@ -28,117 +28,166 @@ double vx = 0, vy = 0;
 // Initialize forces
 double fx = 0, fy = 0;
 
-pid_t watchdog_pid;
-pid_t process_id;
-char *process_name;
-struct timeval prev_t;
-char logfile_name[256] = LOG_FILE_NAME;
-char command;
+pid_t watchdog_pid;                     // Process ID for the watchdog process
+pid_t process_id;                       // Process ID for the current process
+char *process_name;                     // Name of the current process
+struct timeval prev_t;                  // Previous time value
+char logfile_name[256] = LOG_FILE_NAME; // Log file name
+char command;                           // Command character
 
-struct Drone
+struct Drone // Define a Drone structure
 {
     int x;
     int y;
     char symbol;
     short color_pair;
 };
-struct Drone drone;
+struct Drone drone; // Create a Drone instance
 
-int pipefd[2];
-fd_set read_fds;
-struct timeval tv;
-int retval;
+int pipefd[2];     // File descriptors for the pipe
+fd_set read_fds;   // Set of file descriptors for select()
+struct timeval tv; // Time value for select()
+int retval;        // Return value
 
 // logs time update to file
 void log_receipt(struct timeval tv)
 {
-    FILE *lf_fp = fopen(logfile_name, "a");
-    fprintf(lf_fp, "%d %ld %ld\n", process_id, tv.tv_sec, tv.tv_usec);
-    fclose(lf_fp);
-}
-
-void watchdog_handler(int sig, siginfo_t *info, void *context)
-{
-    if (info->si_pid == watchdog_pid)
+    FILE *lf_fp = fopen(logfile_name, "a"); // Open the log file
+    if (lf_fp == NULL)
     {
-        gettimeofday(&prev_t, NULL);
-        log_receipt(prev_t);
+        perror("fopen"); // Print an error message if the file can't be opened
+        return;
+    }
+    fprintf(lf_fp, "%d %ld %ld\n", process_id, tv.tv_sec, tv.tv_usec); // Write the process ID and time to the log file
+    int result = fclose(lf_fp);                                        // Close the log file
+    if (result != 0)
+    {
+        perror("fclose"); // Print an error message if the file can't be closed
     }
 }
 
-int main(int argc, char *argv[])
+void watchdog_handler(int sig, siginfo_t *info, void *context) // Signal handler for the watchdog process
 {
-    // Set up sigaction for receiving signals from processes
+    if (info->si_pid == watchdog_pid) // If the signal was sent by the watchdog process
+    {
+        gettimeofday(&prev_t, NULL); // Get the current time
+        log_receipt(prev_t);         // Log the receipt of the signal
+    }
+}
+
+int main(int argc, char *argv[]) // Main function
+{
+    // Define a signal set
+    sigset_t set;
+
+    // Initialize the signal set to empty
+    sigemptyset(&set);
+
+    // Add SIGUSR1 to the set
+    sigaddset(&set, SIGUSR1);
+
+    // Block SIGUSR1
+    if (sigprocmask(SIG_BLOCK, &set, NULL) < 0)
+    {
+        perror("sigprocmask"); // Print an error message if the signal can't be blocked
+        return -1;
+    }
+    // Set up sigaction for receiving signals from the watchdog process
     struct sigaction p_action;
     p_action.sa_flags = SA_SIGINFO;
     p_action.sa_sigaction = watchdog_handler;
     if (sigaction(SIGUSR1, &p_action, NULL) < 0)
     {
-        perror("sigaction");
+        perror("sigaction"); // Print an error message if the signal can't be set up
     }
 
+    // Unblock SIGUSR1
+    if (sigprocmask(SIG_UNBLOCK, &set, NULL) < 0)
+    {
+        perror("sigprocmask"); // Print an error message if the signal can't be unblocked
+        return -1;
+    }
+
+    // Process command line arguments
     int process_num;
     if (argc == 4)
     {
-        sscanf(argv[1], "%d", &process_num);
-        sscanf(argv[2], "%d", &pipefd[PIPE_READ]);
-        sscanf(argv[3], "%d", &pipefd[PIPE_WRITE]);
+        sscanf(argv[1], "%d", &process_num);        // Get the process number
+        sscanf(argv[2], "%d", &pipefd[PIPE_READ]);  // Get the read end of the pipe
+        sscanf(argv[3], "%d", &pipefd[PIPE_WRITE]); // Get the write end of the pipe
     }
     else
     {
-        printf("wrong args\n");
+        printf("wrong args\n"); // Print an error message if the arguments are incorrect
         return -1;
     }
 
-    printf("pipefd[0] = %d, pipefd[1] = %d\n", pipefd[PIPE_READ], pipefd[PIPE_WRITE]);
-    // close(pipefd[PIPE_WRITE]);
+    printf("pipefd[0] = %d, pipefd[1] = %d\n", pipefd[PIPE_READ], pipefd[PIPE_WRITE]); // Print the file descriptors for the pipe
+    // close(pipefd[PIPE_WRITE]); // Close the write end of the pipe
 
+    // Initialize the screen
     initscr();
-    cbreak();
-    noecho();
-    keypad(stdscr, TRUE);
+    cbreak();             // Disable line buffering
+    noecho();             // Disable echoing of input characters
+    keypad(stdscr, TRUE); // Enable the keypad of the user's terminal
 
     // Publish your pid
-    process_id = getpid();
+    process_id = getpid(); // Get the process ID
 
-    char *fnames[NUM_PROCESSES] = PID_FILE_SP;
+    char *fnames[NUM_PROCESSES] = PID_FILE_SP; // Array of file names
 
-    FILE *pid_fp = fopen(fnames[process_num], "w");
-    fprintf(pid_fp, "%d", process_id);
-    fclose(pid_fp);
-
-    printf("Published pid %d \n", process_id);
+    FILE *pid_fp = fopen(fnames[process_num], "w"); // Open the file for writing
+    if (pid_fp == NULL)                             // If the file couldn't be opened
+    {
+        perror("fopen"); // Print an error message
+        return -1;       // Return an error code
+    }
+    fprintf(pid_fp, "%d", process_id); // Write the process ID to the file
+    int result = fclose(pid_fp);       // Close the file
+    if (result != 0)                   // If the file couldn't be closed
+    {
+        perror("fclose"); // Print an error message
+    }
+    printf("Published pid %d \n", process_id); // Print the process ID
 
     // Read watchdog pid
-    FILE *watchdog_fp = NULL;
-    struct stat sbuf;
+    FILE *watchdog_fp = NULL; // File pointer for the watchdog process
+    struct stat sbuf;         // Stat structure for file information
 
     /* call stat, fill stat buffer, validate success */
-    if (stat(PID_FILE_PW, &sbuf) == -1)
+    if (stat(PID_FILE_PW, &sbuf) == -1) // If the stat call fails
     {
-        perror("error-stat");
-        return -1;
+        perror("error-stat"); // Print an error message
+        return -1;            // Return an error code
     }
     // waits until the file has data
-    while (sbuf.st_size <= 0)
+    while (sbuf.st_size <= 0) // While the file is empty
     {
-        if (stat(PID_FILE_PW, &sbuf) == -1)
+        if (stat(PID_FILE_PW, &sbuf) == -1) // If the stat call fails
         {
-            perror("error-stat");
-            return -1;
+            perror("error-stat"); // Print an error message
+            return -1;            // Return an error code
         }
-        usleep(50000);
+        usleep(50000); // Sleep for 50 milliseconds
     }
 
-    watchdog_fp = fopen(PID_FILE_PW, "r");
-
-    fscanf(watchdog_fp, "%d", &watchdog_pid);
-    printf("watchdog pid %d \n", watchdog_pid);
-    fclose(watchdog_fp);
+    watchdog_fp = fopen(PID_FILE_PW, "r"); // Open the file for reading
+    if (watchdog_fp == NULL)               // If the file couldn't be opened
+    {
+        perror("fopen"); // Print an error message
+        return -1;       // Return an error code
+    }
+    fscanf(watchdog_fp, "%d", &watchdog_pid);   // Read the watchdog process ID from the file
+    printf("watchdog pid %d \n", watchdog_pid); // Print the watchdog process ID
+    result = fclose(watchdog_fp);               // Close the file
+    if (result != 0)                            // If the file couldn't be closed
+    {
+        perror("fclose"); // Print an error message
+    }
 
     // Read how long to sleep process for
-    int sleep_durations[NUM_PROCESSES] = PROCESS_SLEEPS_US;
-    int sleep_duration = sleep_durations[process_num];
+    int sleep_durations[NUM_PROCESSES] = PROCESS_SLEEPS_US; // Array of sleep durations
+    int sleep_duration = sleep_durations[process_num];      // Sleep duration for the current process
     char *process_names[NUM_PROCESSES] = PROCESS_NAMES;
     process_name = process_names[process_num]; // added to logfile for readability
 
@@ -161,7 +210,7 @@ int main(int argc, char *argv[])
     FILE *file = fopen("file_para.txt", "r");
     if (file == NULL)
     {
-        printf("Could not open file_para.txt for reading\n");
+        perror("Could not open file_para.txt for reading\n");
         return 1;
     }
     struct Drone drone;
@@ -196,7 +245,11 @@ int main(int argc, char *argv[])
         }
     }
 
-    fclose(file);
+    result = fclose(file);
+    if (result != 0)
+    {
+        perror("fclose");
+    }
 
     printf("drone.symbol = %c\n", drone.symbol);
     printf("drone.color_pair = %hd\n", drone.color_pair);
@@ -204,11 +257,14 @@ int main(int argc, char *argv[])
     printf("K = %f\n", K);
     printf("drone.x = %d\n", drone.x);
     printf("drone.y = %d\n", drone.y);
-    sleep(10);
+    
+    // Assign shared drone parameters
     shared_drone->x = drone.x;
     shared_drone->y = drone.y;
     shared_drone->symbol = drone.symbol;
     shared_drone->color_pair = drone.color_pair;
+    
+    // Initialize variables
     double prev_x = drone.x, prev_y = drone.y;
     double prev_vx = 0, prev_vy = 0;
     double Fx = fx;
@@ -277,7 +333,7 @@ int main(int argc, char *argv[])
         }
         else if (bytesRead == 0)
         {
-            printf("Nothing to read\n");
+            perror("Nothing to read\n");
         }
         else
         {
@@ -320,6 +376,19 @@ int main(int argc, char *argv[])
     if (shmdt(shared_drone) == -1)
     {
         perror("shmdt");
+        return -1;
+    }
+    // Close the read end of the pipe
+    if (close(pipefd[PIPE_READ]) == -1)
+    {
+        perror("close");
+        return -1;
+    }
+
+    // Close the write end of the pipe
+    if (close(pipefd[PIPE_WRITE]) == -1)
+    {
+        perror("close");
         return -1;
     }
 
--- Imple/master.c
+++ Imple/master.c
@@ -15,21 +15,23 @@
 
 int main(int argc, char *argv[])
 {
-    // pids for all children
+    // Declare PIDs for all child processes
     pid_t child_watchdog;
     pid_t child_process0;
     pid_t child_process1;
     pid_t child_process2;
     pid_t child_process3;
 
+    // Prepare the log file name and remove any existing log file
     char logfile_name[256];
     sprintf(logfile_name, LOG_FILE_NAME);
     remove(logfile_name);
 
+    // Open the log file for writing
     FILE *logfile = fopen(logfile_name, "w");
     if (logfile == NULL)
     {
-        perror("fopen");
+        perror("Error opening log file");
         return -1;
     }
 
@@ -37,18 +39,18 @@ int main(int argc, char *argv[])
     int pipefd[2];
     if (pipe(pipefd) == -1)
     {
-        perror("pipe");
+        perror("Error creating pipe");
         return -1;
     }
+    printf("Pipe created successfully\n");
 
+    // Convert the pipe file descriptors to strings
     char pipe_read_str[10];
     char pipe_write_str[10];
     sprintf(pipe_read_str, "%d", pipefd[PIPE_READ]);
     sprintf(pipe_write_str, "%d", pipefd[PIPE_WRITE]);
 
-    // There should be a check that the log folder exists but I haven't done that
-    // sprintf(logfile_name, "./log/watchdog/watchdog%i-%i-%i_%i:%i:%i.txt", timenow->tm_year + 1900, timenow->tm_mon, timenow->tm_mday, timenow->tm_hour, timenow->tm_min, timenow->tm_sec);
-
+    // Open PID files for writing
     fopen(PID_FILE_PW, "w");
     char *fnames[NUM_PROCESSES] = PID_FILE_SP;
 
@@ -59,19 +61,40 @@ int main(int argc, char *argv[])
 
     int res;
     int num_children = 0;
-    
-    // Create watchdog
+
+    // Create the watchdog process
     child_watchdog = fork();
     if (child_watchdog < 0)
     {
         perror("Fork");
         return -1;
     }
+    printf("Watchdog process created successfully with PID %d\n", child_watchdog);
 
+    // If this is the watchdog process, replace it with the watchdog program
     if (child_watchdog == 0)
     {
         char *arg_list[] = {"konsole", "-e", "./watchdog", NULL};
         execvp("konsole", arg_list);
+        perror("execvp failed for watchdog");
+        return 0;
+    }
+    num_children += 1;
+
+    // Create the first child process and replace it with the server program
+    child_process0 = fork();
+    if (child_process0 < 0)
+    {
+        perror("Fork");
+        return -1;
+    }
+    printf("Child process 0 created successfully with PID %d\n", child_process0);
+
+    if (child_process0 == 0)
+    {
+        char *arg_list[] = {"konsole", "-e", "./server", "0", NULL};
+        execvp("konsole", arg_list);
+        perror("execvp failed for watchdog");
         return 0;
     }
     num_children += 1;
@@ -83,69 +106,99 @@ int main(int argc, char *argv[])
         perror("Fork");
         return -1;
     }
+    printf("Child process 0 created successfully with PID %d\n", child_process0);
 
     if (child_process0 == 0)
     {
         char *arg_list[] = {"konsole", "-e", "./server", "0", NULL};
         execvp("konsole", arg_list);
+        perror("execvp failed for child_process0");
         return 0;
     }
     num_children += 1;
 
+    // Create the second child process and replace it with the drone program
     child_process1 = fork();
     if (child_process1 < 0)
     {
-        perror("Fork");
+        perror("Fork"); // Print an error message if fork failed
         return -1;
     }
+    printf("Child process 1 created successfully with PID %d\n", child_process1);
 
     if (child_process1 == 0)
     {
+        // Prepare the argument list for the drone program
         char *arg_list[] = {"konsole", "-e", "./drone", "1", pipe_read_str, pipe_write_str, NULL};
-        execvp("konsole", arg_list);
+        execvp("konsole", arg_list);                // Replace the current process image with the drone program
+        perror("execvp failed for child_process1"); // Print an error message if execvp failed
         return 0;
     }
-    num_children += 1;
+    num_children += 1; // Increment the number of child processes
 
+    // Create the third child process and replace it with the input program
     child_process2 = fork();
     if (child_process2 < 0)
     {
-        perror("Fork");
+        perror("Fork"); // Print an error message if fork failed
         return -1;
     }
+    printf("Child process 2 created successfully with PID %d\n", child_process2);
 
     if (child_process2 == 0)
     {
+        // Prepare the argument list for the input program
         char *arg_list[] = {"konsole", "-e", "./input", "2", pipe_read_str, pipe_write_str, NULL};
-        execvp("konsole", arg_list);
+        execvp("konsole", arg_list);                // Replace the current process image with the input program
+        perror("execvp failed for child_process2"); // Print an error message if execvp failed
         return 0;
     }
-    num_children += 1;
+    num_children += 1; // Increment the number of child processes
 
+    // Create the fourth child process and replace it with the world program
     child_process3 = fork();
     if (child_process3 < 0)
     {
-        perror("Fork");
+        perror("Fork"); // Print an error message if fork failed
         return -1;
     }
+    printf("Child process 3 created successfully with PID %d\n", child_process3);
 
     if (child_process3 == 0)
     {
+        // Prepare the argument list for the world program
         char *arg_list[] = {"konsole", "-e", "./world", "3", NULL};
-        execvp("konsole", arg_list);
+        execvp("konsole", arg_list);                // Replace the current process image with the world program
+        perror("execvp failed for child_process3"); // Print an error message if execvp failed
         return 0;
     }
-    num_children += 1;
+    num_children += 1; // Increment the number of child processes
 
-    // Wait for all children to die
+    // Wait for all child processes to terminate
+    int status;
+    int last_non_zero_status = 0;
     for (int i = 0; i < num_children; i++)
     {
-        wait(&res);
+        pid_t pid = wait(&status); // Wait for a child process to terminate
+        if (WIFEXITED(status))     // If the child process terminated normally
+        {
+            printf("Child process %d exited with status %d\n", pid, WEXITSTATUS(status)); // Print the PID and exit status of the terminated child process
+            if (WEXITSTATUS(status) != 0)                                                 // If the exit status is not 0 (indicating an error)
+            {
+                last_non_zero_status = WEXITSTATUS(status); // Store the non-zero exit status
+            }
+        }
+        else // If the child process did not terminate normally
+        {
+            printf("Child process %d did not exit successfully\n", pid); // Print an error message
+        }
     }
+    printf("All child processes terminated\n"); // Print a message indicating that all child processes have terminated
 
-    close(pipefd[0]);
-    close(pipefd[1]);
-    printf("pipefd[0] = %d, pipefd[1] = %d\n", pipefd[PIPE_READ], pipefd[PIPE_WRITE]);
+    close(pipefd[0]);                                                                  // Close the read end of the pipe
+    close(pipefd[1]);                                                                  // Close the write end of the pipe
+    printf("pipefd[0] = %d, pipefd[1] = %d\n", pipefd[PIPE_READ], pipefd[PIPE_WRITE]); // Print the file descriptors of the pipe ends
+    printf("Pipe closed successfully\n");                                              // Print a message indicating that the pipe has been closed successfully
 
-    return 0;
+    return last_non_zero_status; // Return the last non-zero exit status of the child processes
 }
