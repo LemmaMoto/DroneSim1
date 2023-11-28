6f438b448fe1e48fb680efca9e135a338b7072de
FedeMala00 <104447347+FedeMala00@users.noreply.github.com>
2023-11-28 12:57:49 +0100

    Update run.sh

--- Imple/run.sh
+++ Imple/run.sh
@@ -1,6 +1,17 @@
+# Navigate to the build directory
 cd build
+
+# Remove any existing compiled binaries
 rm -f ./master ./drone ./input ./server ./watchdog ./world
+
+# Navigate back to the parent directory
 cd ..
+
+# Compile all programs
 make all
+
+# Navigate to the build directory
 cd build
+
+# Run the master program
 ./master
