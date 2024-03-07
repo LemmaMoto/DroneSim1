744c701247826b7c7723ee3b2a7f7c80d7b62130
LemmaMoto <emanuelebuaodetti@gmail.com>
2024-03-07 16:12:30 +0100

    qwertyu

--- Imple/run.sh
+++ Imple/run.sh
@@ -1,17 +1,28 @@
-# Navigate to the build directory
-cd build
+#!/bin/bash
 
-# Remove any existing compiled binaries
-rm -f ./master ./drone ./input ./obstacles ./server ./targets ./watchdog ./world
+# Crea la cartella build_process se non esiste
+mkdir -p bin
+mkdir -p lib
+mkdir -p log
 
-# Navigate back to the parent directory
-cd ..
+# Compila la libreria arplib
+gcc -c -o lib/arplib.o src/arplib.c
+ar rcs lib/libarplib.a lib/arplib.o
 
-# Compile all programs
-make all
+# Compila i processi con le relative dipendenze
+gcc -o bin/master src/master.c -L./lib -larplib
+gcc -o bin/server src/server.c -L./lib -larplib -lncurses -lm
+gcc -o bin/input src/input.c -L./lib -larplib -lncurses
+# gcc -o bin/socket_server src/socket_server.c -L./lib -larplib -lncurses
+gcc -o bin/socket src/socket.c -L./lib -larplib -lncurses
+gcc -o bin/drone src/drone.c -L./lib -larplib
+gcc -o bin/target src/target.c -L./lib -larplib
+gcc -o bin/obstacle src/obstacle.c -L./lib -larplib
+gcc -o bin/rule_print src/rule_print.c -L./lib -larplib -lncurses
+gcc -o bin/wd src/wd.c -L./lib -larplib
 
-# Navigate back to the build directory
-cd build
+# Cambia la directory in build_process
+cd bin
 
-# Run the master program
+# Esegui il processo master
 ./master
